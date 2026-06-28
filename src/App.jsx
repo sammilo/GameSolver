8399530a89912fe11d0d333169398f0e15261079
Samantha Milo <milo.samantha18@gmail.com>
2026-06-28 00:47:24 -0400

    CSS: Fixed spacing and alignment issues on different screens FEAT: Added all button and drop-down functionarily, and expanded on the solution algorithms.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import { useRef, useState } from 'react'
 import defaultPuzzleImage from './assets/default_puzzle.png'
 import './App.css'
 
-// Main App component, renders the games and metrics, and runs search algorithms
+/* Main App component, renders the games and metrics, and runs search algorithms. */ 
 export default function App() {
-  const [puzzleImage, setPuzzleImage] = useState(defaultPuzzleImage)
+  // Use states and file ref for 8-puzzle
   const fileInputRef = useRef(null)
+  const [puzzleImage, setPuzzleImage] = useState(defaultPuzzleImage)
+  const [puzzleAlgo, setPuzzleAlgo] = useState("BFS")
+  const [puzzleMode, setPuzzleMode] = useState("Player")
+  const [puzzleData, setPuzzleData] = useState([[1,2,3],[4,5,6],[7,8,0]])
+
+  // Use states for tictactoe puzzle
+  const [ticTacToeAlgo, setTicTacToeAlgo] = useState("Minimax")
+  const [ticTacToeMode, setTicTacToeMode] = useState("PvAI")
+  const [ticTacToeData, setTicTacToeData] = useState([[0,0,0],[0,0,0],[0,0,0]])
+
+  const [displaySelections, setDisplaySelections] = useState(null)
 
   /* Handle the image upload, image rezising, and imapge update for the 8-puzzle. */
   const handleUploadImage = async(imgFile) => {
@@ -26,6 +37,7 @@ export default function App() {
       image.src = imgURL
     })
 
+    // Reize/crop uploaded image so that it's always 576x576 without stretching
     const canvas = document.createElement('canvas')
     canvas.width = 576
     canvas.height = 576
@@ -53,9 +65,8 @@ export default function App() {
 
             <div className="btn-group">
               <button className="reset-button">◼</button>
-              <button className="solve-button">▶︎</button>
+              <button className="solve-button" onClick={() => handleSolveClick(puzzleAlgo, puzzleMode, puzzleData)}>▶︎</button>
               <button className="upload-img-button" onClick={() => fileInputRef.current?.click()}>🖿</button>
-              
               <button className="remove-img-button" onClick={() => setPuzzleImage(defaultPuzzleImage)}>✖</button>
             </div>
 
@@ -66,28 +77,31 @@ export default function App() {
               <div className="dropdown-container">
                 <button className="dropdown-btn">Algo ▼</button>
                 <div className="dropdown">
-                  <div className="dropdown-option">BFS</div>
-                  <div className="dropdown-option">Dijkstra</div>
-                  <div className="dropdown-option">A*</div>
+                  <div className="dropdown-option" onClick={() => setPuzzleAlgo("BFS")}>BFS</div>
+                  <div className="dropdown-option" onClick={() => setPuzzleAlgo("Dijkstra")}>Dijkstra</div>
+                  <div className="dropdown-option" onClick={() => setPuzzleAlgo("A*")}>A*</div>
                 </div>
               </div>
               <div className="dropdown-container">
                 <button className="dropdown-btn">Mode ▼</button>
                 <div className="dropdown">
-                  <div className="dropdown-option">Player Solves</div>
-                  <div className="dropdown-option">AI Solves</div>
+                  <div className="dropdown-option" onClick={() => setPuzzleMode("Player")}>Player Solves</div>
+                  <div className="dropdown-option" onClick={() => setPuzzleMode("AI")}>AI Solves</div>
                 </div>
               </div>
             </div>
           </div>
 
+          <p> Algorithm and Mode Selected: {puzzleAlgo} , {puzzleMode}</p>
+
           <div className="metrics-container">
             <h3>Metrics</h3>
             <div className="metrics">
-              <p>Time per move:</p>
-              <p>Nodes expanded:</p>
+              <p>Time per move: </p>
+              <p>Nodes expanded: </p>
             </div>
           </div>
+
         </div>
       </div>
       <div className="main-container">
@@ -98,7 +112,7 @@ export default function App() {
             
             <div className="btn-group">
               <button className="reset-button">◼</button>
-              <button className="solve-button">▶︎</button>
+              <button className="solve-button" onClick={() => handleSolveClick(ticTacToeAlgo, ticTacToeMode, ticTacToeData)}>▶︎</button>
             </div>
 
             <div className="tictactoe-placeholder"></div>
@@ -107,25 +121,28 @@ export default function App() {
               <div className="dropdown-container">
                 <button className="dropdown-btn">Algo ▼</button>
                 <div className="dropdown">
-                  <div className="dropdown-option">Minimax</div>
-                  <div className="dropdown-option">Alpha-Beta</div>
+                  <div className="dropdown-option" onClick={() => setTicTacToeAlgo("Minimax")}>Minimax</div>
+                  <div className="dropdown-option" onClick={() => setTicTacToeAlgo("AlphaBeta")}>Alpha-Beta</div>
                 </div>
               </div>
               <div className="dropdown-container">
                 <button className="dropdown-btn">Mode ▼</button>
                 <div className="dropdown">
-                  <div className="dropdown-option">Player vs AI</div>
-                  <div className="dropdown-option">AI vs AI</div>
+                  <div className="dropdown-option" onClick={() => setTicTacToeMode("PvsAI")}>Player vs. AI</div>
+                  <div className="dropdown-option" onClick={() => setTicTacToeMode("AIvsAI")}>AI vs. AI</div>
                 </div>
               </div>
             </div>
           </div>
 
+          <p> Algorithm and Mode Selected: {ticTacToeAlgo} , {ticTacToeMode}</p>
+
           <div className="metrics-container">
             <h3>Metrics</h3>
             <div className="metrics">
-              <p>Time per move:</p>
-              <p>Nodes expanded:</p>
+              <p>Time per move: </p>
+              <p>Nodes expanded: </p>
+              <p>Puning efficiency: </p>
             </div>
           </div>
         </div>
@@ -135,30 +152,29 @@ export default function App() {
   )
 }
 
-function handleReset(game) {
-  // Reset the game state
-}
-
-function handleSolve(game, algorithm, data) {
+function handleSolveClick(algo, mode, data) {
   // Run the selected search algorithm on the selected game
-  if (game === 'puzzle') {
-    if (algorithm === 'BFS') {
+  if (mode === "Player") {
+    //solvePlayer(data)
+  }
+  if (mode === "AI") {
+    if (algo === 'BFS') {
       BFS(data)
-    } else if (algorithm === 'Dijkstra') {
+    } else if (algo === 'Dijkstra') {
       Dijkstra(data)
-    } else if (algorithm === 'A*') {
+    } else {
       AStar(data)
     }
-  } else if (game === 'tictactoe') {
-    if (algorithm === 'Minimax') {
-      Minimax(data)
-    } else if (algorithm === 'Alpha-Beta') {
-      AlphaBeta(data)
+  }
+  
+  if (mode === "PvsAI" || mode === "AIvsAI") {
+    if (algo === 'Minimax') {
+      Minimax(mode, data)
+    } else {
+      AlphaBeta(mode, data)
     }
   }
 }
-
-
 
 function BFS(data) {
   // Implement BFS algorithm to solve the puzzle
